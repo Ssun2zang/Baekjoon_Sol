c379f1bc04b79d71caa96e9ee84e11a8d6fe1319
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-15 14:26:10 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,25 +1,17 @@
-k = int(input())
+n = int(input())
 cnt = 0
-
-for i in range(0, k):
-    answkd = input()
-    answkd_len = len(answkd)
+for i in range(n):
+    str = input()
     arr = []
-    arr_break = True
-    for j in range(1, answkd_len):
-        for k in range(0, len(arr)):
-            if (answkd[j] == arr[k]):
-                arr_break = False
+    temp = ''
+    flag = 1
+    for j in range(len(str)):
+        if temp != str[j]:
+            temp = str[j]
+            if str[j] in arr:
+                flag = 0
                 break
-
-        if(arr_break == False):
-            break    
-
-        if (answkd[j-1] == answkd[j]):
-            pass
-        else:
-            arr.append(answkd[j-1])
-    if(arr_break == True):
-        cnt += 1
-
+            else:
+                arr.append(str[j])
+    cnt+=flag
 print(cnt)
