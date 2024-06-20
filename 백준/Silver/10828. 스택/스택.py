fb8674f6e729afdadc14ebb55784a8b2745eced5
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-21 00:19:42 +0900

    [Silver IV] Title: 스택, Time: 112 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+
+input=sys.stdin.readline
+
+n = int(input())
+cmd = {'push':0, 'pop':1, 'size':2, 'empty': 3, 'top':4}
+_cmd = -1
+val = -1
+d = deque()
+for i in range(n):
+    _msg = input().split()
+    if len(_msg) == 1:
+        _cmd = cmd[_msg[0]]
+    else:
+        _cmd = cmd[_msg[0]]
+        val = int(_msg[1])
+    if (_cmd == 0):
+        d.append(val)
+    elif (_cmd == 1):
+        if (not d):
+            print(-1)
+        else:
+            print(d.pop())
+    elif (_cmd == 2):
+        print(len(d))
+    elif (_cmd == 3):
+        if (not d):
+            print(1)
+        else:
+            print(0)
+    elif (_cmd == 4):
+        if (not d):
+            print(-1)
+        else:
+            print(d[-1])
