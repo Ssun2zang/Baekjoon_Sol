7a712809a82c7095939ef5ca37dd1a81d215c2a6
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-16 02:08:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 528 ms, Memory: 32736 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -21,27 +21,23 @@ class Graph:
 
 
 n, m, start = map(int, input().split())
-dfs_g = Graph()
+g = Graph()
 
 # 그래프에 edge를 저장함
 for i in range(m):
     u, v = map(int, input().split())
-    dfs_g.add_edge(u, v)
+    g.add_edge(u, v)
 
-if start not in dfs_g.get_graph().keys():
+if start not in g.get_graph().keys():
     print(start)
     print(start)
 else:
-
-    # pop을 해야하니까 bfs_g를 따로 저장함
-    bfs_g = copy.deepcopy(dfs_g)
-
     # DFS (깊이 우선 탐색)
     visited = []
 
     def dfs(s):
         visited.append(s)
-        for temp in sorted(dfs_g.get_graph()[s]):
+        for temp in sorted(g.get_graph()[s]):
             if temp in visited:
                 pass
             else:
@@ -57,7 +53,7 @@ else:
         while queue:
             t = queue.pop(0)
             bfs_ans.append(t)
-            for temp in sorted(bfs_g.get_graph()[t]):
+            for temp in sorted(g.get_graph()[t]):
                 if temp not in bfs_ans and temp not in queue:
                     queue.append(temp)
 
