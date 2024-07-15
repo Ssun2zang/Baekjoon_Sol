fbc20ad5a1e54f8248f52d998f1d7b53609b7aa6
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-16 01:59:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 536 ms, Memory: 32632 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,66 @@
+import copy
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u in self.graph:
+            self.graph[u].append(v)
+        else:
+            # 정점이 작은 것부터 먼저 방문하기 때문에 heapq을 사용하여 저장함
+            self.graph[u] = [v]
+        if v in self.graph:
+            self.graph[v].append(u)
+        else:
+            # 정점이 작은 것부터 먼저 방문하기 때문에 heapq을 사용하여 저장함
+            self.graph[v] = [u]
+    
+    def get_graph(self):
+        return self.graph
+
+
+n, m, start = map(int, input().split())
+dfs_g = Graph()
+
+# 그래프에 edge를 저장함
+for i in range(m):
+    u, v = map(int, input().split())
+    dfs_g.add_edge(u, v)
+
+if start not in dfs_g.get_graph().keys():
+    print(start)
+    print(start)
+else:
+
+    # pop을 해야하니까 bfs_g를 따로 저장함
+    bfs_g = copy.deepcopy(dfs_g)
+
+    # DFS (깊이 우선 탐색)
+    visited = []
+
+    def dfs(s):
+        visited.append(s)
+        for temp in sorted(dfs_g.get_graph()[s]):
+            if temp in visited:
+                pass
+            else:
+                dfs(temp)   
+
+    dfs(start)
+    print(*visited)
+
+    # BFS (넓이 우선 탐색)
+    bfs_ans = []
+    def bfs(s):
+        queue = [s]
+        while queue:
+            t = queue.pop(0)
+            bfs_ans.append(t)
+            for temp in sorted(bfs_g.get_graph()[t]):
+                if temp not in bfs_ans and temp not in queue:
+                    queue.append(temp)
+
+
+    bfs(start)
+    print(*bfs_ans)
