18c23fcda1e4bfca4d6e4d158efc901973f13da5
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-09-18 22:33:01 +0900

    [Silver I] Title: 회의실 배정, Time: 2584 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,28 @@
+# N개의 회의가 있음
+# 시작과 끝나는 시간이 주어짐
+# 끝나는 동시에 다음 회의 가능
+# 시작시간과 끝나는 시간이 같을 수 있음
+
+# 가능한 최대 회의 개수 구하기
+
+# 끝나는 시각을 기준으로 그리디하면 될 것 같은데...
+
+N = int(input())
+
+meetings = []
+
+for _ in range(N):
+    start, end = map(int, input().split())
+    meetings.append((start, end))
+
+meetings.sort(key= lambda x: (x[1], x[0]))
+
+answer = 1
+cur = meetings[0][1]
+
+for i, meeting in enumerate(meetings[1:]):
+    if meeting[0] >= cur:
+        answer +=1
+        cur = meeting[1]
+
+print(answer)
